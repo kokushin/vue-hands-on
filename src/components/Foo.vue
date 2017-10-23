24ed3318d676662fb722f9cf0fdee949f4d46e50
kokushin <mail@kokush.in>
2017-10-23 12:32:39 +0900

    コンポーネントを作ってみる 1, 2

---
+++ src/components/Foo.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Foo</h1>
+</template>
+
+<script>
+  console.log('foo')
+</script>
+
+<style scoped lang="scss">
+  // scoped で ローカルスコープ化
+  // lang="scss" でSCSS記法を有効化 要node-sass, sass-loader
+  h1 {
+    color: #333;
+  }
+</style>
