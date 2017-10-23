67d501f0067f33b112fdf119558acbac7154781b
kokushin <mail@kokush.in>
2017-10-23 12:34:28 +0900

    コンポーネントを作ってみる 3, 4

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Foo from '@/components/Foo'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/foo',
+      name: 'Foo',
+      component: Foo
     }
   ]
 })
